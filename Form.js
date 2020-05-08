3be7ca2af8920bb9c94f1ee055114e9e3a3cd87a
ARAVIND281 <63240570+ARAVIND281@users.noreply.github.com>
2020-05-08 20:05:09 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,56 @@
+class Form {
+    constructor(){
+       }
+    display(){
+        var title = createElement('h2');
+        title.html("SURVEY");
+        title.position(130,0);
+        var question1 = createElement('h3');
+        question1.html("Do you think we need to have free lunch meals in our school canteen for the kids who are very poor");
+        question1.position(130,160);
+        var input1 = createInput("YES OR NO");
+        input1.position(130,230);
+        var button1 = createButton('SUBMET');
+        button1.position(250,255);
+        button1.mousePressed(function () {
+            question1.hide();
+            input1.hide();
+            button1.hide();
+            player.updateName(question1);
+            var question2 = createElement('h3');
+       question2.html("Do you think we need to have free lunch meals in our school canteen for the kids who are very poorWould you be willing to contribute a small amount every month for such a program?");
+       question2.position(130,160);
+        var input2 = createInput("YES OR NO");
+        input2.position(130,230);
+        var button2 = createButton('SUBMET');
+        button2.position(250,255);
+        button2.mousePressed(function () {
+            question2.hide();
+            input2.hide();
+            button2.hide();
+            player.updateName(question2);
+            var question3 = createElement('h3');
+            question3.html("How much per month would you be willing to pay?");
+            question3.position(130,160);
+            var input3 = createInput("100,500,1000 OR MORE");
+            input3.position(130,190);
+            var button3 = createButton('SUBMET');
+            button3.position(250,230);
+            var greeting = createElement('h3');
+            button3.mousePressed(function () {
+                question3.hide();
+                input3.hide();
+                button3.hide();
+                playerCount = playerCount+1;
+                player.updateName(question3);
+                player.updateCount(playerCount);
+                greeting.html("THANK YOU YOUR SURVEY");
+                greeting.position(130,160);
+           })
+       })
+       })
+       
+      /* */
+       
+    }   
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# SURVEY
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
